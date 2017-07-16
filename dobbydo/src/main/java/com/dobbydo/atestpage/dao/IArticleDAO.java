d18df5a87f579eceef75469bce2272c31326b9bf
alpha <alpha@a>
2017-07-16 23:28:45 +0900

    First Commit

---
+++ dobbydo/src/main/java/com/dobbydo/atestpage/dao/IArticleDAO.java
@@ -0,0 +1,13 @@
+package com.dobbydo.atestpage.dao;
+import java.util.List;
+
+import com.dobbydo.atestpage.entity.Article;
+public interface IArticleDAO {
+    List<Article> getAllArticles();
+    Article getArticleById(int articleId);
+    void createArticle(Article article);
+    void updateArticle(Article article);
+    void deleteArticle(int articleId);
+    boolean articleExists(String title, String category);
+}
+ 
