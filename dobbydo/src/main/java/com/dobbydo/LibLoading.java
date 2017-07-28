4dde948171b3a57e937901a11d11a98d52a06749
alpha <alpha@a>
2017-07-28 22:35:11 +0900

    Commit!

--- dobbydo/src/main/java/com/dobbydo/LibLoading.java
+++ dobbydo/src/main/java/com/dobbydo/LibLoading.java
@@ -7,6 +7,6 @@ public class LibLoading {
 	static {
 		//System.setProperty("java.library.path", "C:\\Users\\Kait\\git\\dobbydo\\dobbydo\\libs");
 		//System.loadLibrary( "opencv_java320.dll" );    
-		System.load( "C:\\Users\\Kait\\git\\dobbydo\\dobbydo\\libs\\opencv_java320.dll" );
+		//System.load( "C:\\Users\\Kait\\git\\dobbydo\\dobbydo\\libs\\opencv_java320.dll" );
 	}
 }
